e98d9a9fca1ce1825d4c907f3e1dc68acb56f96a
ManhIT <manhit96@gmail.com>
2022-10-08 00:56:36 +0700

    add: eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: [],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
